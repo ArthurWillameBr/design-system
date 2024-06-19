eb00e84ad169deee16c5df75f16122f54c139e18
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-19 15:40:15 -0300

    chore: add react

--- packages/react/src/index.ts
+++
@@ -1,2 +0,0 @@
-import { colors } from "@arthur-ui/tokens"
-
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,7 @@
+import { colors } from "@arthur-ui/tokens"
+
+export function App() {
+    return (
+        <h1 style={{color: colors.ignite500}}>Hello, World</h1>
+    )
+}
