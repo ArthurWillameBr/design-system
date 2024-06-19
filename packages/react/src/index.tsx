1d25f7846a4e43df91ce271d7f5982e99f7f7474
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-19 15:55:02 -0300

    chore: add stitches to styles

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,6 @@
-import { colors } from "@arthur-ui/tokens"
 
 export function App() {
     return (
-        <h1 style={{color: colors.ignite500}}>Hello, World</h1>
+        <h1>Hello, World</h1>
     )
 }
